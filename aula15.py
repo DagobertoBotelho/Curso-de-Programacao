c4de6932a7edefd58ea641aeddddbe1c980fc188
DagobertoBotelho <146292818+DagobertoBotelho@users.noreply.github.com>
2023-10-09 21:54:04 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,9 @@
+# exercicio 1
+
+#valor_menor = float(input("Digite o preço: "))
+
+#for i in range(2):
+#    valor = float(input("Digite o preço: "))
+#    if valor < valor_menor:
+#        valor_menor = valor
+#print("O produto mais em conta é o no valor de: R$",valor_menor)
