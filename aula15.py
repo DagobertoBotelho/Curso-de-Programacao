12c243b524fc6691a74cecb606f1461b5c70220c
DagobertoBotelho <146292818+DagobertoBotelho@users.noreply.github.com>
2023-10-10 21:33:47 -0300

    Update aula15.py

--- aula15.py
+++ aula15.py
@@ -7,3 +7,21 @@
 #    if valor < valor_menor:
 #        valor_menor = valor
 #print("O produto mais em conta é o no valor de: R$",valor_menor)
+
+# exercicio 2
+
+numero = []
+a = float(input("Digite um número: "))
+numero.append(a)
+b = float(input("Digite um número: "))
+numero.append(b)
+c = float(input("Digite um número: "))
+numero.append(c)
+
+maior = max(numero)
+menor = min(numero)
+meio = numero[0] + numero[1] + numero[2] - maior - menor
+
+print(maior)
+print(meio)
+print(menor)
